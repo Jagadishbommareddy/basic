a5228e9d7e7b72e871f71e74f25c41574b807dc2
Rameshbommareddy <pythondevelop12@gmail.com>
2019-02-20 22:15:01 +0530

    changes

--- basic/basic/settings.py
+++ basic/basic/settings.py
@@ -78,12 +78,13 @@ WSGI_APPLICATION = 'basic.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+       'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'basic',
+            'USER':'root',
+            'PASSWORD':'password'
+        }
     }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
